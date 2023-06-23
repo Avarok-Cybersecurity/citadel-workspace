c9c0547d69441a2e7541e0b520f667fd3806a1eb
Radu Cazacu <cazacuraduic@gmail.com>
2023-06-23 19:48:38 +0300

    Installed all rust dep

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod tcp_conn_opener;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn open_tcp_conn_as_peer() -> String {
-    format!("Hi there")
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, open_tcp_conn_as_peer])
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/tcp_conn_opener.rs
@@ -0,0 +1,4 @@
+use citadel_workspace_types::{InternalServicePayload, InternalServiceResponse};
+use tokio::net::TcpStream;
+
+fn open_tcp_conn_as_peer() {}
