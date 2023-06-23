8422693cbeda9bd8f7ff2d348864741fb45312af
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-06-23 13:58:35 -0400

    add send message and display features

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useMemo, useEffect, useState, useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { IMessage } from '@/lib/utils'
+import dayjs from 'dayjs'
+
+import _ from 'lodash'
 
 function Chat() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const [messageInput, setMessageInput] = useState('');
+  const [currentUser, setCurrentUser] = useState('User1');
+
+  const handleSend = () => {
+    // append the new message to the current list of messages
+    setMessages(messages => [...messages, { user: currentUser, message: messageInput, timestamp: new Date() }]);
+    // clear the input field
+    setMessageInput('');
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentUser(event.target.value);
+  }
   return (
     <div className="">
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}><b>{message.user}:</b> {message.message} <i>{message.timestamp.toLocaleTimeString()}</i></p>
+        ))}
+      </div>
       <form>
         <div className="flex">
           <label
@@ -75,18 +99,29 @@ function Chat() {
             </ul>
           </div>
           <div className="relative w-full">
+            
+            {/* <input
+              type="text"
+              value={currentUser}
+              onChange={handleUserChange}
+              placeholder="Your name..."
+            /> */}
             <input
               type="search"
               id="search-dropdown"
               className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
               placeholder="Message"
+              value={messageInput}
+              onChange={(e) => setMessageInput(e.target.value)}
             />
             <button
               type="button"
               className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={handleSend}
             >
               Send
             </button>
+
           </div>
         </div>
       </form>
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export interface User {
+  email: string
+  username: string
+  online?: boolean
+}
+
+export interface UsersState {
+  users: User[]
+  onlineUsersByUsername: string[]
+  loading: boolean
+  error: string | null,
+  typingUsers: string[]
+}
+
+// =======================================================================================
+// Messages
+// =======================================================================================
+
+export interface IMessage {
+  user: string;
+  message: string;
+  timestamp: Date;
+}
