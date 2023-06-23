41b4f2157b44caad4862c4ad2e8aeace20a1e1fb
Radu Cazacu <cazacuraduic@gmail.com>
2023-06-23 19:59:20 +0300

    Minimal code for opening a TCP conn

--- src-tauri/src/tcp_conn_opener.rs
+++ src-tauri/src/tcp_conn_opener.rs
@@ -1,4 +1,20 @@
 use citadel_workspace_types::{InternalServicePayload, InternalServiceResponse};
+use futures::{StreamExt, TryStreamExt};
+use std::net::SocketAddr;
 use tokio::net::TcpStream;
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
-fn open_tcp_conn_as_peer() {}
+pub fn wrap_tcp_conn(conn: TcpStream) -> Framed<TcpStream, LengthDelimitedCodec> {
+    LengthDelimitedCodec::builder()
+        .length_field_offset(0) // default value
+        .max_frame_length(1024 * 1024 * 64) // 64 MB
+        .length_field_type::<u32>()
+        .length_adjustment(0) // default value
+        .new_framed(conn)
+}
+
+async fn open_tcp_conn_as_peer(internal_service_addr: SocketAddr) {
+    let conn = TcpStream::connect(internal_service_addr).await.unwrap();
+    let framed = wrap_tcp_conn(conn);
+    let (mut sink, mut stream) = framed.split();
+}
