8c0acce2160d7057b4a83557e13156c58fbd1f09
Radu Cazacu <cazacuraduic@gmail.com>
2023-07-03 23:52:49 +0300

    Err

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,30 +4,24 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 mod lib;
 mod structs;
+use bytes::BytesMut;
 use citadel_workspace_lib::wrap_tcp_conn;
 use citadel_workspace_types::{InternalServicePayload, InternalServiceResponse};
-use futures::StreamExt;
-use lib::send_to_service;
+use futures::{SinkExt, StreamExt};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::time::Duration;
 use structs::ConnectionState;
-use tauri::{window, Event, Manager, State, Window};
+use tauri::{Manager, State};
 use tokio::net::TcpStream;
-use tokio::sync::mpsc;
 use tokio::time::timeout;
+use uuid::Uuid;
 
-#[derive(Debug)]
-enum Listener {
-    Msg,
-    Leave,
-}
-
-#[tauri::command]
-async fn send_payload(
-    payload: InternalServicePayload,
-    state: State<'_, ConnectionState>,
-) -> Result<(), ()> {
-    send_to_service(state, payload).await;
+async fn send_response(packet: BytesMut, window: tauri::Window) -> Result<(), ()> {
+    let _ = window.emit(
+        "packet",
+        serde_json::to_string(&bincode2::deserialize::<InternalServiceResponse>(&packet).unwrap())
+            .unwrap(),
+    );
     Ok(())
 }
 
@@ -50,14 +44,7 @@ async fn open_tcp_conn(
             let (sink, mut stream) = framed.split();
             *conn_state.sink.lock().await = Some(sink);
             while let Some(packet) = stream.next().await {
-                let _ = window.emit(
-                    "packet",
-                    serde_json::to_string(
-                        &bincode2::deserialize::<InternalServiceResponse>(&packet.unwrap())
-                            .unwrap(),
-                    )
-                    .unwrap(),
-                );
+                let _ = send_response(packet.unwrap(), window.clone()).await;
             }
             Ok(format!("Connected"))
         }
@@ -73,6 +60,29 @@ async fn main() {
             stream: Default::default(),
         })
         .setup(|app| {
+            let app_handle = app.handle();
+            tauri::async_runtime::spawn(async move {
+                let state = app_handle.state::<ConnectionState>().clone();
+                let sink = state.sink.lock().await.as_mut().unwrap();
+                let id = app_handle.listen_global("event-name", |event| {
+                    tauri::async_runtime::spawn(async move {
+                        println!("got event-name with payload {:?}", event.payload());
+                        send_to_internal_service(
+                            InternalServicePayload::Connect {
+                                uuid: Uuid::new_v4(),
+                                username: "Test".to_string(),
+                                password: "Pass".into(),
+                                connect_mode: Default::default(),
+                                udp_mode: Default::default(),
+                                keep_alive_timeout: Default::default(),
+                                session_security_settings: Default::default(),
+                            },
+                            sink,
+                        )
+                        .await;
+                    });
+                });
+            });
             #[cfg(debug_assertions)] // only include this code on debug builds
             {
                 let window = app.get_window("main").unwrap();
@@ -84,4 +94,16 @@ async fn main() {
         .invoke_handler(tauri::generate_handler![open_tcp_conn])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn send_to_internal_service<R: tauri::Runtime>(
+    payload: InternalServicePayload,
+    sink: &mut futures::stream::SplitSink<
+        tokio_util::codec::Framed<TcpStream, tokio_util::codec::LengthDelimitedCodec>,
+        bytes::Bytes,
+    >,
+) {
+    let _ = sink
+        .send(bincode2::serialize(&payload).unwrap().into())
+        .await;
 }
