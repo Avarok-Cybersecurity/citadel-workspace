e1c73cff8bb9613bf92117786781c999efb1f646
Radu Cazacu <cazacuraduic@gmail.com>
2023-08-02 15:58:11 +0300

    Trying to add mobile support to tauri

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-};
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig;
+module.exports = async (phase, { defaultConfig }) => {
+  let internalHost = null;
+  if (!isProd) {
+    const { internalIpV4 } = await import('internal-ip');
+    internalHost = await internalIpV4();
+  }
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'export',
+    // Note: This experimental feature is required to use NextJS Image in SSG mode.
+    // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: isProd ? null : `http://${internalHost}:3000`,
+  };
+  return nextConfig;
+};
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+export default {
+  devServer: {
+    host: 'local-ipv4',
+  },
+};
