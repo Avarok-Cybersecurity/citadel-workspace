2f24ea028202b5b5585bea8066ec2b891e1f03ee
Radu Cazacu <cazacuraduic@gmail.com>
2023-09-26 00:52:51 +0300

    Fix recursive rerendering

--- framework/app/utils/invoke-api.ts
+++ framework/app/utils/invoke-api.ts
@@ -1,7 +1,7 @@
 import { ApiInvokeTypes, Variables } from '@common/types/api';
 import { invoke } from '@tauri-apps/api/tauri';
 
-const invokeApi = async <T>(type: ApiInvokeTypes, variables: Variables) => {
+const invokeApi = async (type: ApiInvokeTypes, variables: Variables) => {
   try {
     console.log('Type of the command', type);
     const data = await invoke(type, variables);
--- pages/server/findPeers/[findPeersServerCid].tsx
+++ pages/server/findPeers/[findPeersServerCid].tsx
@@ -1,6 +1,7 @@
 import { Layout } from '@components/common/Layout';
 import { useGetAllPeers_c2s } from '@framework/c2s';
 import { useAppSelector } from 'framework/redux/store';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const FindPeers = () => {
@@ -8,14 +9,16 @@ const FindPeers = () => {
     (state) => state.context.sessions.current_used_session_server
   );
 
+  const router = useRouter();
   const { uuid } = useAppSelector((state) => state.uuid);
 
   const getAllPeers = useGetAllPeers_c2s();
-  getAllPeers({ uuid, cid: Number(currentSessionInUse) });
+  if (!uuid) router.replace('/');
 
   const peers = useAppSelector((state) => state.context.peers);
 
   useEffect(() => {
+    getAllPeers({ uuid, cid: Number(currentSessionInUse) });
     console.log('Smth', peers);
   }, []);
 
