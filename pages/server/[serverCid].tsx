4d395ba212d042bc4df86a20ccaffd0fc5f27efe
Radu Cazacu <cazacuraduic@gmail.com>
2024-01-25 21:38:57 +0200

    Added ListAllPeers when entering in a server

--- pages/server/[serverCid].tsx
+++ pages/server/[serverCid].tsx
@@ -1,5 +1,17 @@
 import { Layout } from '@components/common/Layout';
+import useListAllPeers from '@hooks/c2s/useListAllPeers';
+import { useAppSelector } from '@redux/store';
+import { useEffect } from 'react';
 const Server = () => {
+  const current_selected_session = useAppSelector(
+    (state) => state.context.sessions.current_used_session_server
+  );
+
+  useEffect(() => {
+    useListAllPeers({ cid: current_selected_session });
+    return () => {};
+  }, [current_selected_session]);
+
   return (
     <div className="text-4xl text-teal-50 text-center h-[calc(100vh-5.5rem)] select-none">
       <h1>Welcome to the Citadel Server</h1>
