b0d745edc929fba650674b9a3c29b0f22ffd7529
Kyle Tennison <kyletennison05@gmail.com>
2024-06-08 22:38:26 -0700

    chore: cargo fmt

--- src-tauri/src/structs.rs
+++ src-tauri/src/structs.rs
@@ -1,10 +1,10 @@
 use citadel_internal_service_connector::connector::WrappedSink;
+use citadel_internal_service_connector::io_interface::tcp::TcpIOInterface;
 use citadel_internal_service_types::InternalServiceResponse;
 use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
-use citadel_internal_service_connector::io_interface::tcp::TcpIOInterface;
 
-pub struct ConnectionState{
+pub struct ConnectionState {
     pub sink: Mutex<Option<WrappedSink<TcpIOInterface>>>,
 }
 #[derive(Serialize, Deserialize, Debug)]
