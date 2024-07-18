2ca631e4ae98f039bf4ef0aada85cdde0de66509
Kyle Tennison <kyletennison05@gmail.com>
2024-07-17 18:06:15 -0700

    wip: placeholder registration page

---
+++ src/elements/pages/landing/landing.tsx
@@ -0,0 +1,17 @@
+/*
+
+Landing page for users who are first logging in
+
+*/
+
+import "./landing.css"
+
+export default function Landing() {
+    
+    return <div id="landing">
+        <h1>Registration</h1>
+        <p>This page is a placeholder for a more sophisticated registration page</p>
+
+        <button>Add Workspace</button>
+    </div>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,27 @@ import ReactDOM from "react-dom/client";
 import Home from "./elements/pages/home/home"
 import "./main.css"
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Landing from "./elements/pages/landing/landing";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
