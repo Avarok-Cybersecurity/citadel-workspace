f981f10f7f9c1cbbfb6e4b2f0189a481048e30a0
Radu Cazacu <cazacuraduic@gmail.com>
2024-12-02 02:57:23 +0200

    Compiling version

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-
+  esbuild: {
+    target: "esnext",
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
